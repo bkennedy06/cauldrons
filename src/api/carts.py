afb35008ccf09cef0f732eb3c764904f16c6a07b
bkennedy06 <bkennedyeble@gmail.com>
2024-05-08 15:51:09 -0700

    Forgot one

--- src/api/carts.py
+++ src/api/carts.py
@@ -63,7 +63,7 @@ def search_orders(
         if customer_name != "" and potion_sku != "": # Both searching
             result = connection.execute(sqlalchemy.text("SELECT potion_type, potion_quantity_change, cust_name, created_at, id FROM ledger WHERE potion_type = :type and cust_name = :name and description = 'Potions sold'"), {'type' : potion_sku, 'name' : customer_name})
         elif potion_sku == "" and customer_name != "": # search for customer name
-            result = connection.execute(sqlalchemy.text("SELECT potion_type, potion_quantity_change, cust_name, created_at, id FROM ledger WHERE cust_name = :name"), {'name' : customer_name})
+            result = connection.execute(sqlalchemy.text("SELECT potion_type, potion_quantity_change, cust_name, created_at, id FROM ledger WHERE cust_name = :name and description = 'Potions sold'"), {'name' : customer_name})
         elif customer_name == "" and potion_sku != "": # search for potion_sku
             result = connection.execute(sqlalchemy.text("SELECT potion_type, potion_quantity_change, cust_name, created_at, id FROM ledger WHERE potion_type = :type and description = 'Potions sold'"), {'type' : potion_sku})
         else: # No search
